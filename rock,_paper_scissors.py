642657fbdf5bc1e69c8b150984bd549ea648672c
Sabarno Chatterjee <106575210+Sabarno-Chatterjee@users.noreply.github.com>
2022-09-14 20:05:07 +0530

    Add files via upload

--- rock,_paper_scissors.py
+++ rock,_paper_scissors.py
@@ -32,15 +32,19 @@ comp = random.randint(1, 3)
 print(comp)
 user = input("Please enter 1 for rock, 2 for paper or 3 for scissor\n")
 user = int(user)
-if (comp == 1 or comp == 3) and (user == 3 or user == 1):
+if comp == 1 and user == 3:
     print("Rock wins over scissor.")
-elif (comp == 2 or comp == 1) and (user == 1 or user == 2):
+elif comp == 3 and user == 1:
+    print("Rock wins over scissor.")
+elif comp == 2 and user == 1:
     print("Paper wins over rock.")
-elif (comp == 3 or comp == 2) and (user == 2 or user == 3):
+elif comp == 1 and user == 2:
+    print("Paper wins over rock.")
+elif comp == 3 and user == 2:
+    print("Scissor wins over paper.")
+elif comp == 2 and user == 3:
     print("Scissor wins over paper.")
 elif comp == user:
     print("Draw.")
-'''
 else:
     print("Invalid input, run the game again!")
-'''
